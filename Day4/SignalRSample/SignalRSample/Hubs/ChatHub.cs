6ff41f496724ca2395d2944cfc89b3cbf7c9f46e
Christian Nagel <christian@christiannagel.com>
2019-01-24 13:45:10 +0100

    crnl and encoding

--- Day4/SignalRSample/SignalRSample/Hubs/ChatHub.cs
+++ Day4/SignalRSample/SignalRSample/Hubs/ChatHub.cs
@@ -2,7 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace SignalRSample.Hubs
 {
@@ -11,6 +14,9 @@ namespace SignalRSample.Hubs
 
         public async Task SendMessage(string name, string message)
         {
+          
+            name = HttpUtility.HtmlEncode(name);
+            message = HttpUtility.HtmlEncode(message);
             await base.Clients.All.SendAsync("BroadcastMessage", name, message);
         }
     }
