1f7ea3199b9a7f3aa1d7092465b1c6f0f65c98a5
Christian Nagel <christian@christiannagel.com>
2019-01-24 13:39:44 +0100

    signalr

---
+++ Day4/SignalRSample/SignalRSample/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRSample.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        public async Task SendMessage(string name, string message)
+        {
+            await base.Clients.All.SendAsync("BroadcastMessage", name, message);
+        }
+    }
+}
